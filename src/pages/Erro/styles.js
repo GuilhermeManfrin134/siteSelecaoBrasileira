ca10790493488cb071b318bf4c042232a679253c
GuilhermeManfrin134 <manfrin11gui@gmail.com>
2022-05-01 21:15:08 -0300

    Corrigindo bug da página Erro

--- src/pages/Erro/styles.js
+++ src/pages/Erro/styles.js
@@ -8,6 +8,7 @@ export const ErroBr = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    margin-bottom: 350px;
 `;
 
 export const TextError = styled.div`
